d1a0d859a21b475a0f8d52b5a302abe0526337f1
kijoo-l <kijoo1128@yonsei.ac.kr>
2025-05-08 09:49:49 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,62 @@
+const data = [
+	{
+	  id: 1, // 상품 아이디인데, 순서대로 1, 2, 3 하시면 됩니다
+	  name: '캣타워', // 상품 이름
+	  price: 150_000, // 상품 가격
+	  image: 'https://picsum.photos/200' // 상품 이미지 주소
+	},
+	{
+	  id: 2,
+	  name: '츄르 10개입',
+	  price: 8_000,
+	  image: 'https://picsum.photos/200'
+	},
+	{
+	  id: 3,
+	  name: '스크래쳐',
+	  price: 30_000,
+	  image: 'https://picsum.photos/200'
+	},
+	{
+	  id: 4,
+	  name: '고양이 낚싯대',
+	  price: 8_000,
+	  image: 'https://picsum.photos/200'
+	},
+	{
+	  id: 5,
+	  name: '숨숨집',
+	  price: 45_000,
+	  image: 'https://picsum.photos/200'
+	},
+    {
+        id: 6,
+        name: '고양이 화장실',
+        price: 30_000,
+        image: 'https://picsum.photos/200'
+      },
+      {
+        id: 7,
+        name: '고양이 화장실 모래 4kg',
+        price: 20_000,
+        image: 'https://picsum.photos/200'
+      },
+      {
+        id: 8,
+        name: '빗',
+        price: 8_000,
+        image: 'https://picsum.photos/200'
+      },
+      {
+        id: 9,
+        name: '고양이 구강케어 칫솔+치약 세트',
+        price: 15_000,
+        image: 'https://picsum.photos/200'
+      },
+      {
+        id: 10,
+        name: '캣 휠',
+        price: 100_000,
+        image: 'https://picsum.photos/200'
+      },
+]
